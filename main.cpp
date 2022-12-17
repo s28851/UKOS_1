92b2a89f2cec0b600707d0d399281e7f6e3a2b06
Marek Draczyński <s28851@pjwstk.edu.pl>
2022-12-17 13:07:54 +0100

    mnozenie

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int multiply(int 1, intb);
 int main(){
-    int a;
+    int a=2, b=10;
+    cout << multiply(a,b);
     return 0;
+}
+
+int multiply(int 1, intb){
+    return a*b;
 }
